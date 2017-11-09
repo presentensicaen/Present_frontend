f37a97a7f2c0b70992d739d965daaafd3c947319
jeanneLeymarie <jeanne.leymarie35@gmail.com>
2017-11-09 17:13:27 +0100

    generateCode(init activite)

---
+++ app/src/main/java/fr/ensicaen/present/present/GenerateCodeActivity.java
@@ -0,0 +1,13 @@
+package fr.ensicaen.present.present;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GenerateCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_code);
+    }
+}
