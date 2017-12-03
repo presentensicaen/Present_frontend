597318caf90a7e89d76725430022264b679570d1
Pierre Nicol <pierre.nicol22@gmail.com>
2017-12-03 23:03:21 +0100

    ajout d'un listener sur le bouton pour faire apparaitre le green check

--- app/src/main/java/fr/ensicaen/present/present/ConfigureCall.java
+++ app/src/main/java/fr/ensicaen/present/present/ConfigureCall.java
@@ -2,6 +2,8 @@ package fr.ensicaen.present.present;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ConfigureCall extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class ConfigureCall extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configure_call);
+
+
+        Button button1 = (Button)findViewById(R.id.launchCall);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                findViewById(R.id.successCall).setVisibility(View.VISIBLE);
+            }
+        });
     }
+
+
 }
