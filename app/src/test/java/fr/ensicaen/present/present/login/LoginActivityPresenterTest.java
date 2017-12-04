a7962ef424121038a33ed57fba1aa3311a394271
Julian EASTERLY <julianeasterly@gmail.com>
2017-12-04 18:54:46 +0100

    tests(): added tests files that need to be filled for the Login and Splash Presnter

---
+++ app/src/test/java/fr/ensicaen/present/present/login/LoginActivityPresenterTest.java
@@ -0,0 +1,17 @@
+package fr.ensicaen.present.present.login;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by jueast on 04/12/17.
+ */
+
+public class LoginActivityPresenterTest {
+
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
