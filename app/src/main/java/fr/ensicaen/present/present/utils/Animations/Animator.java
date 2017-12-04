e08f0dbdcda4b940fd39f71734f3df1ff8087225
Julian EASTERLY <julianeasterly@gmail.com>
2017-12-04 18:54:18 +0100

    animations(): added a chain of command to handle animations

---
+++ app/src/main/java/fr/ensicaen/present/present/utils/Animations/Animator.java
@@ -0,0 +1,23 @@
+package fr.ensicaen.present.present.utils.Animations;
+
+import android.view.View;
+
+/**
+ * Created by jueast on 04/12/17.
+ */
+
+public class Animator implements IAnimation {
+
+    private AbstractAnimationCOR _root;
+
+    public Animator(){
+        _root = new ButtonAnimation();
+        _root.setSuccessor(new TextInputAnimation());
+    }
+
+    @Override
+    public void animate(View v, int delay) {
+        _root.processRequest(v, delay);
+    }
+
+}
