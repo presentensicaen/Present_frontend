54a0e476a481b76a71f3e1b25d2f1478aa80d4fe
Pierre Nicol <pierre.nicol22@gmail.com>
2017-12-31 18:03:33 +0100

    creation of a model for a call

---
+++ app/src/main/java/fr/ensicaen/present/present/models/CallModel.java
@@ -0,0 +1,16 @@
+package fr.ensicaen.present.present.models;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by pierr on 31/12/2017.
+ */
+
+public class CallModel {
+    @SerializedName("id") private String _id;
+    @SerializedName("duration") private String _duration;
+    @SerializedName("groups") private String _groups[] ;
+
+
+
+}
