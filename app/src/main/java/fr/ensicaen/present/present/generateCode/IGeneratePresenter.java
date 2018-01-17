26e045ba68ae40fc3c26b03cca10f4365af8e581
Pierre <pierre.nicol22@gmail.com>
2018-01-17 15:45:38 +0100

    hotfix file duplication

--- app/src/main/java/fr/ensicaen/present/present/generateCode/IGeneratePresenter.java
+++
@@ -1,9 +0,0 @@
-package fr.ensicaen.present.present.generateCode;
-
-/**
- * Created by pierr on 31/12/2017.
- */
-
-public interface IGeneratePresenter {
-    void onDestroy();
-}
--- app/src/main/java/fr/ensicaen/present/present/generateCode/IGenerateView.java
+++
@@ -1,8 +0,0 @@
-package fr.ensicaen.present.present.generateCode;
-
-/**
- * Created by pierr on 31/12/2017.
- */
-
-public interface IGenerateView {
-}
