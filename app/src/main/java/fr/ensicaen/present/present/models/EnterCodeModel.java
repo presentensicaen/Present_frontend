abcbd682af898ecb45c05e3c3c9519eb42091479
jeanneLeymarie <jeanne.leymarie35@gmail.com>
2018-01-17 17:07:54 +0100

    added constructor + toString method to EnterCodeModel class

--- app/src/main/java/fr/ensicaen/present/present/models/EnterCodeModel.java
+++ app/src/main/java/fr/ensicaen/present/present/models/EnterCodeModel.java
@@ -1,8 +1,25 @@
 package fr.ensicaen.present.present.models;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Jeanne on 17/01/2018.
  */
 
 public class EnterCodeModel {
+    @SerializedName("id") private String _id;
+    @SerializedName("code") private String _code;
+
+    public EnterCodeModel(String _id, String _code){
+        this._id = _id;
+        this._code = _code;
+    }
+
+    @Override
+    public String toString(){
+        return "EnterCodeModel{" +
+                "_id='" + _id + '\'' +
+                "_code='" + _code + '\'' +
+                '}';
+    }
 }
