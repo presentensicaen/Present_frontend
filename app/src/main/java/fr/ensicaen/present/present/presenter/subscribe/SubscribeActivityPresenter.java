028ba8469c0c4e496997b138f6366ceafeab4246
seeker <jeanne.leymarie35@gmail.com>
2018-03-07 13:53:17 +0100

    added constructor (presenter)

--- app/src/main/java/fr/ensicaen/present/present/presenter/subscribe/SubscribeActivityPresenter.java
+++ app/src/main/java/fr/ensicaen/present/present/presenter/subscribe/SubscribeActivityPresenter.java
@@ -1,8 +1,17 @@
 package fr.ensicaen.present.present.presenter.subscribe;
 
+import android.view.View;
+
+import fr.ensicaen.present.present.view.subscribe.ISubscribeView;
+
 /**
  * Created by leymarie on 07/03/18.
  */
 
-public class SubscribeActivityPresenter {
+public class SubscribeActivityPresenter implements ISubscribePresenter {
+    private ISubscribeView _view;
+
+    public SubscribeActivityPresenter(ISubscribeView view){
+        _view = view;
+    }
 }
